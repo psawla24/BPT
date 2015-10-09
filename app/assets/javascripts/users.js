d9a8ebb8569780e1830b3b8b533806c27dcc98a2
Pooja <pooja.sawla@gmail.com>
2015-10-08 21:55:08 -0700

    refactor code for entire app

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
   var hello = $('#title');
 
   var backgrounds = new Array(
-      'url(assets/bpt1.jpg)',
-      'url(assets/bpt2.jpg)',
-      'url(assets/bpt3.jpg)',
-      'url(assets/bpt4.jpg)',
-      'url(assets/bpt5.jpg)',
-      'url(assets/bpt6.jpg)',
-      'url(assets/bpt7.jpg)',
-      'url(assets/bpt8.jpg)'
+      'url(https://s3-us-west-1.amazonaws.com/bpt-assets/bpt1.jpg)',
+      'url(https://s3-us-west-1.amazonaws.com/bpt-assets/bpt2.jpg)',
+      'url(https://s3-us-west-1.amazonaws.com/bpt-assets/bpt3.jpg)',
+      'url(https://s3-us-west-1.amazonaws.com/bpt-assets/bpt4.jpg)',
+      'url(https://s3-us-west-1.amazonaws.com/bpt-assets/bpt5.jpg)',
+      'url(https://s3-us-west-1.amazonaws.com/bpt-assets/bpt6.jpg)',
+      'url(https://s3-us-west-1.amazonaws.com/bpt-assets/bpt7.jpg)',
+      'url(https://s3-us-west-1.amazonaws.com/bpt-assets/bpt8.jpg)'
     );
 
   var current = 0;
