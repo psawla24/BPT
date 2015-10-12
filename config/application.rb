ea992e9ae051ee704eb21886ed89441abf023a73
Pooja <pooja.sawla@gmail.com>
2015-10-11 17:15:24 -0700

    refactor code for entire app

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
-require "rails/mongoid"
+# require "rails/mongoid"
 
 # Mongoid.load!('config/mongoid.yml')
 
